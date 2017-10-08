d52a8b4f6b97acf407ec88b0c4d03ef3a19c2eb2
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2017-10-08 17:23:22 +0200

    Button Scrolling, mobile fixed background

--- js/functionality.js
+++ js/functionality.js
@@ -4,10 +4,11 @@ let abilitiesContainer = document.querySelector('.abilities-container');
 
 let abilitiesItemArray = document.querySelectorAll('.abilities-item');
 
+let headerButtons = document.querySelectorAll('.spot');
+
 nameCardContainer.style.height = window.innerHeight;
 meContainer.style.height = window.innerHeight;
 
-console.log(abilitiesContainer.getBoundingClientRect().height);
 if(abilitiesContainer.getBoundingClientRect().height < window.innerHeight){
     abilitiesContainer.style.height = window.innerHeight;
 }
@@ -19,10 +20,19 @@ window.addEventListener('scroll', function(){
 });
 
 function checkAbilitiesItemIsVisible(abilitiesItem){
-    console.log('if ' + window.scrollY  + ' > ' + ( window.scrollY  - window.innerHeight/1.5 + abilitiesItem.getBoundingClientRect().y + abilitiesItem.getBoundingClientRect().height));
-
     if(window.scrollY >= window.scrollY - window.innerHeight/1.5 + abilitiesItem.getBoundingClientRect().y + abilitiesItem.getBoundingClientRect().height){
         abilitiesItem.classList.remove('abilities-item-invisible');
     }
 }
 
+headerButtons[1].addEventListener('click', function(){
+    window.scroll({ top: document.getElementById('me-container-start').getBoundingClientRect().y, left: 0, behavior: 'smooth' });
+});
+
+headerButtons[2].addEventListener('click', function(){
+    window.scroll({ top: document.getElementById('abilities-container-start').getBoundingClientRect().y, left: 0, behavior: 'smooth' });
+});
+
+headerButtons[3].addEventListener('click', function(){
+    window.scroll({ top: document.getElementById('proyects-container-start').getBoundingClientRect().y, left: 0, behavior: 'smooth' });
+});
